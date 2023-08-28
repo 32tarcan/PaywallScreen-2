5570c28deb3e0f298b1c2f1cd6ce7a337712e555
32tarcan <bahadir.tarcan41@gmail.com>
2023-08-28 13:45:08 +0300

    Initial Commit

---
+++ PaywallScreen-2/PaywallScreen_2App.swift
@@ -0,0 +1,17 @@
+//
+//  PaywallScreen_2App.swift
+//  PaywallScreen-2
+//
+//  Created by Bahadır Tarcan on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct PaywallScreen_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
